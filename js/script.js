cebfa814e00917ba436635b5fcce0345f4c96605
alicjakolodziejczyk <76963444+alicjakolodziejczyk@users.noreply.github.com>
2022-09-19 03:28:18 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -25,35 +25,6 @@ if(GetTheme === "DARK"){
   icon.src = "img/sun.png";
 }
 
-
-
-$('form.ajax').on('submit', function(){
-  var that = $(this),
-    url = that.attr('action'),
-    type = that.attr('method'),
-    data = {};
-
-  that.find('[name]').each(function(index, value){
-    var that = $(this),
-      name = that.attr('name'),
-      value = that.val();
-
-    data[name] = value;
-  });
-
-  $.ajax({
-    url: url,
-    type: type,
-    data: data,
-    success: function(response) {
-      console.log(response);
-    }
-  });
-
-  console.log(data);
-  return false;
-});
-
 $(function() {
     $(".toggle").on("click", function() {
         if ($(".item").hasClass("active")) {
